27c938b79ffb1550cb781ffdba964299606186f8
Vinicius De Castro <vini.cas82@gmail.com>
2024-03-05 10:11:48 +0000

    add the base, index and css base templates

--- blog/models.py
+++ blog/models.py
@@ -19,7 +19,7 @@ class Post(models.Model):
     class Meta:
         ordering = ["-created_on"]
     def __str__(self):
-        return f"The title of this post is {self.title} | written by {selt.author}"
+        return f"The title of this post is {self.title} | written by {self.author}"
 
 
 class Comment(models.Model):
